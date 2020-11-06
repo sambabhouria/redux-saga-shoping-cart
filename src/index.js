f8660c8da9cd46861be71a298e566dae73f0cf67
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-11-06 10:58:44 +0100

    shopping cart with redux saga

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import App from './components/App'
+
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from './reducers'
+import rootSaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+  <App />
+</Provider>,
   document.getElementById('root')
 );
 
---
+++ src/services/products.js
@@ -0,0 +1,7 @@
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    { id: 1, title: 'iPad 4 Mini', price: 500.01, inventory: 2 },
+    { id: 2, title: 'H&M T-Shirt White', price: 10.99, inventory: 10 },
+    { id: 3, title: 'Charli XCX - Sucker CD', price: 19.99, inventory: 5 },
+  ]
